d4cf85dbd52bda20c51b2dc5ed75492676a5118f
Manish Kumar <rmanish7836@gmail.com>
2026-01-05 16:12:11 +0530

    overwrite the default timeout of circuitbreaker in gatewayserver

--- section10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ section10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -5,10 +5,16 @@ import java.time.LocalDateTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -45,5 +51,14 @@ public class GatewayserverApplication {
 									.filters(f -> f.rewritePath("/eazybank/cards/(?<remaning>.*)","/${remaning}")
 											.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 								.uri("lb://CARDS")).build();
+	}
+
+	//change the default timeout of circuitbreaker from 1 sec
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id->new Resilience4JConfigBuilder(id)
+								.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+								.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
 	}
 }
