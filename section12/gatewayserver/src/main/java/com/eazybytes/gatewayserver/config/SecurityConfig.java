7e9b59166d3910aaf04fe62d39fe7f2850ef90e1
Manish Kumar <rmanish7836@gmail.com>
2026-01-12 23:35:06 +0530

    added dependency and api patter who required auth and who does not

---
+++ section12/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.eazybytes.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+        // serverHttpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated()) // api to be authenticated,  .anyExchange() --> all api endpoint
+        // serverHttpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().permitAll()) //permit all without authentication,    .anyExchange() --> all api endpoint
+
+        //We want some api which does not require authentication where as for some we what authentication.
+
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll() //permit without authentication for all GET request.
+                                                    .pathMatchers("/eazybank/accounts/**").authenticated() //required authentication if pattern matched. //As the GET is 1st, then GET'll have high priority means even if current pattern matced with GET one.
+                                                    .pathMatchers("/eazybank/cards/**").authenticated()  //required authentication if pattern matched. //As the GET is 1st, then GET'll have high priority means even if current pattern matced with GET one.
+                                                    .pathMatchers("/eazybank/loans/**").authenticated())  //required authentication if pattern matched. //As the GET is 1st, then GET'll have high priority means even if current pattern matced with GET one.
+                                                    .oauth2ResourceServer(oAuth2ResourceServerSpec->oAuth2ResourceServerSpec
+                                                                            .jwt(Customizer.withDefaults())); //to use jwt and use default configuration
+
+        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable()); //disabling csrf protection which is enabled by default in spring security. Useful if browser/UI based application is there. //if not disabled all request of post(), put(), delete() will fail.
+        
+        return serverHttpSecurity.build();
+
+    }
+}
