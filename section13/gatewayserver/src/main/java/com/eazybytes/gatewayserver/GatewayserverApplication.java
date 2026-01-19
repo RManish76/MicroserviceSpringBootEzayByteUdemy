f4f3a9069d7190e21909769bf4f5e5e2ac078399
Manish Kumar <rmanish7836@gmail.com>
2026-01-19 19:51:36 +0530

    trying to increase circuit breaker timout from 4sec to 10 sec

--- section13/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ section13/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -66,7 +66,7 @@ public class GatewayserverApplication {
 	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer(){
 		return factory -> factory.configureDefault(id->new Resilience4JConfigBuilder(id)
 								.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-								.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+								.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(20)).build()).build());
 	}
 
 	//we need to start the redis database or redis container to use this
